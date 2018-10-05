a4279d414b0cabe0097fc681edf423a968d63845
zis <desert73eagle@gmail.com>
2018-10-05 14:27:56 +0300

    Initial Commit

---
+++ Table View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by student on 05.10.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
